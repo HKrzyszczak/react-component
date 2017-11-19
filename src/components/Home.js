7348255f8b83b4c6bc37d7a5989f4fec39df7b65
HKrzyszczak <h.krzyszczak@electriclight.pl>
2017-11-19 08:57:40 +0100

    add button and columns

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import {Grid, Col, Row} from 'react-bootstrap';
 
 const Home = () => {
 
@@ -8,12 +10,19 @@ const Home = () => {
 
     return (
         <div>
-            <button onClick={handleClick}>Button</button>
+            <Button
+                bsStyle="primary"
+                bsSize="large"
+                onClick={handleClick}>Button
+            </Button>
+            <Grid>
+                <Row>
+                    <Col xs={12} md={8}>Column 1</Col>
+                    <Col xs={6} md={4}>Column 2</Col>
+                </Row>
+            </Grid>
         </div>
     )
-
 };
-
-
 
 export default Home;
