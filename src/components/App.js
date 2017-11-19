a3e3e2750e346e9190dc466f5d2331300c4a4137
HKrzyszczak <h.krzyszczak@electriclight.pl>
2017-11-19 10:58:42 +0100

    navigation in menu

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,16 @@ import Home from './Home.js';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import Profile from './Profie.js';
 import Dashboard from './Dashboard.js'
+import Menu from './Menu.js';
 
 import Footer from './Footer.js';
 
 
 const App = () => {
-    // return (
-    //     <div>
-    //         <Header/>
-    //         <Home/>
-    //         <Footer/>
-    //     </div>
-    // );
-
-    return (
+      return (
         <Router>
             <div>
                 <Header/>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/daschboard">Dashboard</Link></li>
-                </ul>
                 <Route exact path="/" component={Home}/>
                 <Route path="/profile" component={Profile}/>
                 <Route path="/dashboard" component={Dashboard}/>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Nav, NavItem} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const Menu = () => {
     return (
-        <Nav bsStyle="pills"
-             activeKey={0}>
-            <NavItem eventKey={0}>Home</NavItem>
-            <NavItem eventKey={1}>Profile</NavItem>
-            <NavItem eventKey={2}>Dashboard</NavItem>
-        </Nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/dashboard">Daschboard</Link></li>
+        </ul>
     );
 };
 
